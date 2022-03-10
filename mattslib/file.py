a15246055b3a896ab2bcfdb7b9b84556dbf30068
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-10 11:55:07 +1100

    Update file.py

--- mattslib/file.py
+++ mattslib/file.py
@@ -3,48 +3,48 @@ import logging
 
 __file__ = 'file'
 __version__ = '1.3'
-__date__ = '06/03/2022'
+__date__ = '10/03/2022'
 
 
 def read(file=""):
-	try:
-		extension = file[-4:]
-		if "txt" in extension:
-			with open(file, "r") as f:
-				contents = f.readlines()
-		elif "json" in extension:
-			with open(file,) as f:
-				contents = json.load(f)
-		f.close()
-		return contents
-	except Exception as e:
-		logging.exception(e)
-	return None
+    try:
+        extension = file[-4:]
+        if "txt" in extension:
+            with open(file, "r") as f:
+                contents = f.readlines()
+        elif "json" in extension:
+            with open(file,) as f:
+                contents = json.load(f)
+        f.close()
+        return contents
+    except Exception as e:
+        logging.exception(e)
+    return None
 
 
 def write(contents=None, file=""):
-	if contents is None:
-		contents = []
-	try:
-		with open(file, "w") as f:
-			if ".txt" in file:
-				f.writelines(contents)
-			elif ".json" in file:
-				if not isinstance(contents, dict):
-					json.dump(contents.__dict__, f)
-				else:
-					json.dump(contents, f)
-		f.close()
-	except Exception as e:
-		logging.exception(e)
+    if contents is None:
+        contents = []
+    try:
+        with open(file, "w") as f:
+            if ".txt" in file:
+                f.writelines(contents)
+            elif ".json" in file:
+                if not isinstance(contents, dict):
+                    json.dump(contents.__dict__, f)
+                else:
+                    json.dump(contents, f)
+        f.close()
+    except Exception as e:
+        logging.exception(e)
 
 
 def append(file="", contents=None):
-	if contents is None:
-		contents = []
-	try:
-		with open(file, "a") as f:
-			f.writelines(contents)
-		f.close()
-	except Exception as e:
-		logging.exception(e)
+    if contents is None:
+        contents = []
+    try:
+        with open(file, "a") as f:
+            f.writelines(contents)
+        f.close()
+    except Exception as e:
+        logging.exception(e)
