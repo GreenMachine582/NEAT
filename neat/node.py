aac490869e581719f1b2a95cc0da98ad1cab5b0a
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-10 12:03:15 +1100

    Update node.py

--- neat/node.py
+++ neat/node.py
@@ -1,11 +1,13 @@
 
 __file__ = 'node'
-__version__ = '1.2'
-__date__ = '02/03/2022'
+__version__ = '1.3'
+__date__ = '08/03/2022'
 
 
 class Node(object):
     def __init__(self, activation):
         self.activation = activation
+        self.depth = 0
         self.output = 0
         self.bias = 0
+        self.backtrack = 0
