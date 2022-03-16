66259966300e9f82794d3bfdbbe151845011d7db
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-16 20:36:14 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NEAT',
-    version='1.3',
+    version='1.4',
     author='Matthew Johnson',
     author_email='greenmachine1902@gmail.com',
     url='https://github.com/greenmachine1902/NEAT',
