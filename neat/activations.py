2142a7e76669707e025b0fc816ddb66df6da6b76
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-17 16:28:33 +1100

    Update activations.py

--- neat/activations.py
+++ neat/activations.py
@@ -1,51 +1,58 @@
+from __future__ import annotations
+
 import math
 import random
 
-__file__ = 'activations'
-__version__ = '1.2'
-__date__ = '02/03/2022'
+__version__ = '1.4.1'
+__date__ = '17/03/2022'
 
 
-def absolute(x):
+def absolute(x: int | float) -> int | float:
     return abs(x)
 
 
-def binaryStep(x):
+def binaryStep(x: int | float) -> int | float:
     return 1 if x >= 0 else 0
 
 
-def clamped(x):
+def clamped(x: int | float) -> int | float:
     return max(-1.0, min(1.0, x))
 
 
-def identity(x):
+def identity(x: int | float) -> int | float:
     return x
 
 
-def log(x):
+def log(x: int | float) -> int | float:
     x = max(1e-7, x)
     return math.log(x)
 
 
-def tanh(x):
+def tanh(x: int | float) -> int | float:
     x = max(-60.0, min(60.0, 2.5 * x))
     return math.tanh(x)
 
 
-def leakyReLU(x):
+def leakyReLU(x: int | float) -> int | float:
     return x if x > 0 else 0.01 * x
 
 
-def sigmoid(x):
+def sigmoid(x: int | float) -> int | float:
     x = max(-60.0, min(60.0, 5 * x))
     return 1 / (1 + math.exp(-x))
 
 
-def swish(x):
+def swish(x: int | float) -> int | float:
     return x * sigmoid(x)
 
 
-def getActivation(activation=''):
+def getActivation(activation: str = '') -> activation_function:
+    """
+    Returns the requested or a random activation function.
+    :param activation: str
+    :return:
+        - activation_function - (x: int | float) -> int | float
+    """
     activations = {'absolute': absolute,
                    'binaryStep': binaryStep,
                    'clamped': clamped,
@@ -55,7 +62,6 @@ def getActivation(activation=''):
                    'leakyReLU': leakyReLU,
                    'sigmoid': sigmoid,
                    'swish': swish}
-    if activation in activations:
-        return activations[activation]
-    else:
-        return random.choice(list(activations.values()))
+    activation_function = activations[activation] if activation in activations \
+        else random.choice(list(activations.values()))
+    return activation_function
