bfb96a5e62651d233fb3dc73deda22447ed4eb9a
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-18 17:10:46 +1100

    Update __init__.py

--- mattslib/__init__.py
+++ mattslib/__init__.py
@@ -1,7 +1,8 @@
 from .list import condense, findMaxMin
 from . import dict
 from . import file
+from . import math_util
 from . import pygame
 
 
-__all__ = ['dict', 'file', 'list', 'condense', 'findMaxMin']
+__all__ = ['dict', 'file', 'list', 'math_util', 'condense', 'findMaxMin']
