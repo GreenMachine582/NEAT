a4113ccadfbf94e91026bd78ad838ec930003899
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-19 12:57:38 +1100

    Update activations.py

--- neat/activations.py
+++ neat/activations.py
@@ -3,47 +3,10 @@ from __future__ import annotations
 import math
 import random
 
-__version__ = '1.4.1'
-__date__ = '17/03/2022'
+from mattslib import math_util
 
-
-def absolute(x: int | float) -> int | float:
-    return abs(x)
-
-
-def binaryStep(x: int | float) -> int | float:
-    return 1 if x >= 0 else 0
-
-
-def clamped(x: int | float) -> int | float:
-    return max(-1.0, min(1.0, x))
-
-
-def identity(x: int | float) -> int | float:
-    return x
-
-
-def log(x: int | float) -> int | float:
-    x = max(1e-7, x)
-    return math.log(x)
-
-
-def tanh(x: int | float) -> int | float:
-    x = max(-60.0, min(60.0, 2.5 * x))
-    return math.tanh(x)
-
-
-def leakyReLU(x: int | float) -> int | float:
-    return x if x > 0 else 0.01 * x
-
-
-def sigmoid(x: int | float) -> int | float:
-    x = max(-60.0, min(60.0, 5 * x))
-    return 1 / (1 + math.exp(-x))
-
-
-def swish(x: int | float) -> int | float:
-    return x * sigmoid(x)
+__version__ = '1.5.1'
+__date__ = '19/03/2022'
 
 
 def getActivation(activation: str = '') -> activation_function:
@@ -53,15 +16,15 @@ def getActivation(activation: str = '') -> activation_function:
     :return:
         - activation_function - (x: int | float) -> int | float
     """
-    activations = {'absolute': absolute,
-                   'binaryStep': binaryStep,
-                   'clamped': clamped,
-                   'identity': identity,
-                   'log': log,
-                   'tanh': tanh,
-                   'leakyReLU': leakyReLU,
-                   'sigmoid': sigmoid,
-                   'swish': swish}
+    activations = {'absolute': math_util.absolute,
+                   'binaryStep': math_util.binaryStep,
+                   'clamped': math_util.clamped,
+                   'identity': math_util.identity,
+                   'log': math_util.log,
+                   'tanh': math_util.tanh,
+                   'leakyReLU': math_util.leakyReLU,
+                   'sigmoid': math_util.sigmoid,
+                   'swish': math_util.swish}
     activation_function = activations[activation] if activation in activations \
         else random.choice(list(activations.values()))
     return activation_function
