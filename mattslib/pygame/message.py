6a674d9074a13407e974d03c88bc54a49ed55cc8
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-19 18:38:49 +1100

    Update message.py

--- mattslib/pygame/message.py
+++ mattslib/pygame/message.py
@@ -1,17 +1,30 @@
-
-__version__ = '1.1'
-__date__ = '16/03/2022'
+from __future__ import annotations
 
 from pygame import font
+
+__version__ = '1.2.1'
+__date__ = '19/03/2022'
+
 
 font.init()
 
 
 class Message(object):
-    def __init__(self, text, pos, colour=(0, 0, 0), size=20, align=''):
+    """
+    Message is an object that handles updating and drawing a message.
+    """
+    def __init__(self, text: Any, pos: tuple, colour: list = None, size: int = 20, align: str = ''):
+        """
+        Initiates the Message object with given values.
+        :param text: Any
+        :param pos: tuple[int, int]
+        :param colour: list[int]
+        :param size: int
+        :param align: str
+        """
         self.text = text
         self.pos = pos
-        self.colour = colour
+        self.colour = colour if colour is not None else [0, 0, 0]
         self.size = size
         self.align = align
         self.font = 'freesansbold.ttf'
@@ -19,20 +32,32 @@ class Message(object):
         self.text_surface = None
         self.text_rect = None
 
-        self.generate()
+        self.update()
 
-    def generate(self, dims=None):
-        padding = 0 if dims is None else (dims[0] / 2) - (self.text_rect[2] / 2)
+    def update(self, dims: list = None) -> None:
+        """
+        Updates the position of the message.
+        :param dims: list[int]
+        :return:
+            - None
+        """
+        padding = 0 if dims is None else int((dims[0] / 2) - (self.text_rect[2] / 2))
 
         text_font = font.Font(self.font, self.size)
         self.text_surface = text_font.render(str(self.text), True, self.colour)
         self.text_rect = self.text_surface.get_rect()
         if self.align == "ml":
-            self.text_rect.midleft = (int(self.pos[0] + padding), self.pos[1])
+            self.text_rect.midleft = (self.pos[0] + padding, self.pos[1])
         elif self.align == "mr":
-            self.text_rect.midright = (int(self.pos[0] - padding), self.pos[1])
+            self.text_rect.midright = (self.pos[0] - padding, self.pos[1])
         else:
             self.text_rect.center = self.pos
 
-    def draw(self, window):
-        window.blit(self.text_surface, self.text_rect)
+    def draw(self, surface: Any) -> None:
+        """
+        Draws that text to the given surface.
+        :param surface: Any
+        :return:
+            - None
+        """
+        surface.blit(self.text_surface, self.text_rect)
