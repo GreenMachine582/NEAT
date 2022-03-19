a5f33a3ec52125c54904d72e95c16164a5a63492
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-19 13:01:01 +1100

    Update math_util.py

--- mattslib/math_util.py
+++ mattslib/math_util.py
@@ -1,10 +1,50 @@
+from __future__ import annotations
+
 import logging
 
-__version__ = '1.1'
-__date__ = '18/03/2022'
+__version__ = '1.2.1'
+__date__ = '19/03/2022'
 
 
-def mean(array=None):
+def absolute(x: int | float) -> int | float:
+    return abs(x)
+
+
+def binaryStep(x: int | float) -> int | float:
+    return 1 if x >= 0 else 0
+
+
+def clamped(x: int | float) -> int | float:
+    return max(-1.0, min(1.0, x))
+
+
+def identity(x: int | float) -> int | float:
+    return x
+
+
+def log(x: int | float) -> int | float:
+    return math.log(max(1e-7, x))
+
+
+def tanh(x: int | float) -> int | float:
+    x = max(-60.0, min(60.0, 2.5 * x))
+    return math.tanh(x)
+
+
+def leakyReLU(x: int | float) -> int | float:
+    return x if x > 0 else 0.01 * x
+
+
+def sigmoid(x: int | float) -> int | float:
+    x = max(-60.0, min(60.0, 5 * x))
+    return 1 / (1 + math.exp(-x))
+
+
+def swish(x: int | float) -> int | float:
+    return x * sigmoid(x)
+
+
+def mean(array: list = None) -> int | float:
     try:
         return sum(array) / len(array)
     except Exception as e:
