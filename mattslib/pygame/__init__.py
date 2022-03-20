7f47b23bc7c3dca73f65776d7cde09ac2fda9b38
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-20 22:44:45 +1100

    Update __init__.py

--- mattslib/pygame/__init__.py
+++ mattslib/pygame/__init__.py
@@ -1,13 +1,6 @@
-from . import shape
-from .button import Button, ButtonGroup
+from .shape import Rect, Circle
+from .button import changeColour, Button, ButtonGroup
 from .message import Message
 
 
-def changeColour(colour, change_by=-70):
-    new_colour = [colour[i] + change_by for i in range(len(colour))]
-    for i in range(len(colour)):
-        new_colour[i] = 0 if new_colour[i] < 0 else 255 if new_colour[i] > 255 else new_colour[i]
-    return new_colour
-
-
-__all__ = ['changeColour', 'shape', 'Button', 'ButtonGroup', 'Message']
+__all__ = ['changeColour', 'Rect', 'Circle', 'Button', 'ButtonGroup', 'Message']
