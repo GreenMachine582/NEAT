6c58adbb7541e000b21be6bafc446d022b449a53
Matthew Johnson <49015217+greenmachine1902@users.noreply.github.com>
2022-03-23 12:37:25 +1100

    Update __init__.py

--- mattslib/pygame/__init__.py
+++ mattslib/pygame/__init__.py
@@ -2,6 +2,9 @@ from .shape import Rect, Circle
 from .button import changeColour, Button, ButtonGroup
 from .message import Message
 
+__all__ = ['changeColour', 'Rect', 'Circle', 'Button', 'ButtonGroup', 'Message']
+__version__ = '1.2'
+
 
 # Colors
 BLACK = [0, 0, 0]
@@ -11,6 +14,3 @@ RED = [255, 0, 0]
 GREEN = [0, 255, 0]
 BLUE = [0, 0, 255]
 YELLOW = [255, 255, 0]
-
-
-__all__ = ['changeColour', 'Rect', 'Circle', 'Button', 'ButtonGroup', 'Message']
