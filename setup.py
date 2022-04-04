ecaa204a341cd3ad33789590b326fda9b43473c9
Matthew <greenchicken1902@gmail.com>
2022-04-04 20:29:04 +1000

    Edited the setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.5',
     author='Matthew Johnson',
     author_email='greenmachine1902@gmail.com',
-    url='https://github.com/greenmachine1902/NEAT',
+    url='https://github.com/GreenMachine582/NEAT',
     license="BSD",
     description='Application of a NEAT with Connect 4',
     long_description='Application of a NEAT (NeuroEvolution of Augmenting Topologies) with a Connect 4 environment',
