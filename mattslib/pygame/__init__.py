efd135f3e89ee526d3f5ae8f49b2e18f452ecf32
Matthew <greenchicken1902@gmail.com>
2022-04-05 09:29:59 +1000

    Added colours

--- mattslib/pygame/__init__.py
+++ mattslib/pygame/__init__.py
@@ -8,8 +8,10 @@ __version__ = '1.2'
 
 # Colors
 BLACK = [0, 0, 0]
+DARK_GRAY = [40, 40, 40]
+GRAY = [100, 100, 100]
+LIGHT_GRAY = [200, 200, 200]
 WHITE = [255, 255, 255]
-GREY = [200, 200, 200]
 RED = [255, 0, 0]
 GREEN = [0, 255, 0]
 BLUE = [0, 0, 255]
