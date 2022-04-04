4bf207220edded8624c58cf8b1ca7de6e6bb4b4a
Matthew <greenchicken1902@gmail.com>
2022-04-04 13:15:16 +1000

    Added the best models

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='NEAT',
-    version='1.4',
+    version='1.5',
     author='Matthew Johnson',
     author_email='greenmachine1902@gmail.com',
     url='https://github.com/greenmachine1902/NEAT',
     license="BSD",
-    description='Connect 4 with NEAT (NeuroEvolution of Augmenting Topologies)',
-    packages=['connect4', 'mattslib', 'neat'],
+    description='Application of a NEAT with Connect 4',
+    long_description='Application of a NEAT (NeuroEvolution of Augmenting Topologies) with a Connect 4 environment',
+    packages=['connect4', 'mattslib', 'mattslib/pygame', 'neat'],
 )
