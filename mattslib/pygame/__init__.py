41c931f116cc052209ffa0b2b713aed7f05e68e2
Matthew <greenchicken1902@gmail.com>
2022-04-07 16:56:56 +1000

    Moved change colour

--- mattslib/pygame/__init__.py
+++ mattslib/pygame/__init__.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 from .shape import Rect, Circle
-from .button import changeColour, Button, ButtonGroup
+from .button import Button, ButtonGroup
 from .message import Message
 
 __all__ = ['changeColour', 'Rect', 'Circle', 'Button', 'ButtonGroup', 'Message']
@@ -21,3 +23,22 @@ DARK_BLUE = [0, 0, 140]
 BLUE = [0, 0, 255]
 DARK_YELLOW = [200, 200, 0]
 YELLOW = [255, 255, 0]
+
+
+def changeColour(colour: list, change_by: Any) -> list:
+    """
+    Changes the given colour with value, the clamps the colour ranges.
+    :param colour: list[int]
+    :param change_by: Any
+    :return:
+        - new_colour - list[int]
+    """
+    if isinstance(change_by, int):
+        new_colour = [colour[i] + change_by for i in range(len(colour))]
+    elif isinstance(change_by, list):
+        new_colour = [colour[i] + change_by[i] for i in range(len(colour))]
+    else:
+        return colour
+    for i in range(len(colour)):
+        new_colour[i] = 0 if new_colour[i] < 0 else 255 if new_colour[i] > 255 else new_colour[i]
+    return new_colour
