d54e441bf5b179adc9db767804dc691119637405
Matthew <greenchicken1902@gmail.com>
2022-04-19 10:39:32 +1000

    Updated documentation, fitness evaluation and logic statements

--- connect4/connect4.py
+++ connect4/connect4.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-from .visualize import GameBoard
-import mattslib.pygame as mlpg
 
-__version__ = '1.4.4'
-__date__ = '6/04/2022'
+__version__ = '1.5.1'
+__date__ = '19/04/2022'
 
 
 class Connect4:
@@ -19,7 +17,7 @@ class Connect4:
     PLAYERS = ['Red', 'Yellow']
     INVALID_MOVE, EMPTY, DRAW, WIN = -2, -1, 0, 1
 
-    def __init__(self, game_dims: tuple = None, **kwargs: Any):
+    def __init__(self):
         """
         Initiates the object with required values.
         :param game_dims: tuple[int | float, int | float]
@@ -29,48 +27,32 @@ class Connect4:
         self.opponent = abs(self.current_player - 1)
         self.match = True
         self.turn = 0
-        self.result = self.EMPTY
 
         self.active = True
-        self.visible = False
 
         self.board = [[self.EMPTY for _ in range(self.COLUMNS)] for _ in range(self.ROWS)]
-        self.game_board = None
 
-        if 'kwargs' in kwargs:
-            kwargs = kwargs['kwargs']
-
-        if game_dims is not None:
-            self.visible = True
-            self.game_board = GameBoard(game_dims, self.ROWS, self.COLUMNS, kwargs=kwargs)
-
-    def reset(self) -> None:
+    def reset(self, switch: bool = True) -> None:
         """
-        Resets key connect4 attributes for next match.
+        Resets attributes in preparation for next match.
+        :param switch: bool
         :return:
             - None
         """
-        self.switchPlayer()
+        if switch:  # defaulted so loser or opponent can go first
+            self.switchPlayer()
         self.match = True
         self.turn = 0
-        self.result = self.EMPTY
-
         self.board = [[self.EMPTY for _ in range(self.COLUMNS)] for _ in range(self.ROWS)]
 
-        if self.visible:
-            self.game_board.update(reset=True)
-
-    def makeMove(self, move: tuple) -> None:
+    def switchPlayer(self) -> None:
         """
-        Receives the move to update the game board and increments turn.
-        :param move: tuple[int, int]
+        Switches the current player with opponent.
         :return:
             - None
         """
-        self.board[move[0]][move[1]] = self.current_player
-        if self.visible:
-            self.game_board.update(move=move, player=self.current_player)
-        self.turn += 1
+        self.current_player = self.opponent
+        self.opponent = abs(self.current_player - 1)
 
     def getPossibleMove(self, possible_move: int) -> tuple:
         """
@@ -86,39 +68,9 @@ class Connect4:
             move = row
         return move, possible_move
 
-    def showWin(self, move: tuple, direction_pair: list, colour: list = mlpg.GREEN) -> None:
-        """
-        Shows the connections made with move that result in a win.
-        :param move: tuple[int, int]
-        :param direction_pair: list[tuple[int, int]]
-        :param colour: list[int]
-        :return:
-            - None
-        """
-        self.game_board.update(move=move, highlight_colour=colour)
-        for direction in direction_pair:
-            for n in range(1, self.LENGTH):
-                a, b = move[0] + (n * direction[0]), move[1] + (n * direction[1])
-                if 0 <= a < self.ROWS and 0 <= b < self.COLUMNS:
-                    if self.board[a][b] == self.current_player:
-                        self.game_board.update(move=(a, b), highlight_colour=colour)
-                    else:
-                        break
-                else:
-                    break
-
-    def switchPlayer(self) -> None:
-        """
-        Switches the current player with opponent.
-        :return:
-            - None
-        """
-        self.current_player = self.opponent
-        self.opponent = abs(self.current_player - 1)
-
     def getPieceSlices(self, move: tuple) -> dict:
         """
-        Gets the piece slices around the move and counts connecting pieces.
+        Gets the piece slices surrounding the move.
         :param move: tuple[int, int]
         :return:
             - directions - dict[str: dict[tuple[int, int]: list[int]]]
@@ -140,7 +92,7 @@ class Connect4:
                             break
         return directions
 
-    def getConnectionCounts(self, directions, player: int = None):
+    def getConnectionCounts(self, directions, player: int = None, immediate_only: bool = True):
         counts = {}
         if player is None:
             player = self.current_player
@@ -150,77 +102,80 @@ class Connect4:
                 connection_count = 0
                 count_connections = True
                 for piece_key in range(1, len(directions[direction_pair][direction])):
-                    if directions[direction_pair][direction][piece_key] == player and count_connections:
+                    piece = directions[direction_pair][direction][piece_key]
+                    if piece == player and count_connections:
                         connection_count += 1
                     else:
-                        count_connections = False
+                        if piece != self.EMPTY or immediate_only:
+                            count_connections = False
                 counts[direction_pair].append(connection_count)
         return counts
 
-    def winChecker(self, move: tuple) -> None:
+    def getBoardStatus(self, move: tuple) -> int:
         """
-        Checks the connections with move for a win, draw or nothing and updates match status.
+        Checks the status of the board and return the result.
         :param move: tuple[int, int]
         :return:
-            - None
+            - result - int
         """
-        win = False
-
+        # Checks for a winning connection
         directions = self.getPieceSlices(move)
         connection_counts = self.getConnectionCounts(directions)
         for direction_pair in directions:
             if sum(connection_counts[direction_pair]) + 1 >= self.LENGTH:
-                if self.visible:
-                    self.showWin(move, directions[direction_pair])
-                win = True
-        if win:
-            self.match = False
-            self.result = self.WIN
-            return
+                return self.WIN
 
+        # Checks for an empty move
         for h in range(self.ROWS):
             for j in range(self.COLUMNS):
                 if self.board[h][j] == self.EMPTY:
-                    self.result = self.EMPTY
-                    return
+                    return self.EMPTY
+        # Draw
+        return self.DRAW
 
-        self.match = False
-        self.result = self.DRAW
-
-    def fitnessEvaluation(self) -> tuple:
+    def fitnessEvaluation(self, move: tuple, args: Any = None) -> int:
         """
-        Calculates the fitness score using match results.
+        Evaluates the fitness using surrounding connections.
+        :param move: tuple[int, int]
+        :param args: Any
         :return:
-            - fitness - tuple[int, int]
+            - fitness - int
         """
-        min_required_moves = (2 * self.LENGTH) - 1
-        winner = max(0, (self.ROWS * self.COLUMNS) - self.turn - min_required_moves)
-        loser = max(0, self.turn - min_required_moves)
-        return winner, loser
+        raw_fitness = {}
+        for i in range(self.COLUMNS):
+            possible_move = self.getPossibleMove(i)
+            if possible_move != self.INVALID_MOVE:
+                directions = self.getPieceSlices(possible_move)
+                score = 0
+                for piece in [self.current_player, self.opponent]:
+                    connection_counts = self.getConnectionCounts(directions, piece, immediate_only=False)
+                    for direction_pair in connection_counts:
+                        score += sum(connection_counts[direction_pair])
+                if score not in raw_fitness:
+                    raw_fitness[score] = []
+                raw_fitness[score].append(possible_move)
 
-    def draw(self, surface: Any) -> None:
+        fitness = {}
+        while raw_fitness:
+            score = max(raw_fitness, key=raw_fitness.get)
+            for possible_move in raw_fitness[score]:
+                fitness[possible_move] = (self.COLUMNS - len(raw_fitness))
+            raw_fitness.pop(score)
+        return fitness[move]
+
+    def main(self, move: tuple) -> None:
         """
-        Draws the game board and pieces to the surface.
-        :param surface: Any
+        Adds the move to board and switches the players turn or concludes
+        match depending on board status results.
+        :param move: tuple[int, int]
         :return:
             - None
         """
-        if self.visible:
-            self.game_board.update(text=f"{self.PLAYERS[self.current_player]}'s turn!")
-            self.game_board.draw(surface)
-
-    def main(self, possible_move: int) -> None:
-        """
-        Checks for possible moves, makes the move, checks board status and then
-        switches player turn.
-        :param possible_move: int
-        :return:
-            - None
-        """
-        if self.active:
-            move = self.getPossibleMove(possible_move)
-            if move[0] != self.INVALID_MOVE:
-                self.makeMove(move)
-                self.winChecker(move)
-                if self.match:
-                    self.switchPlayer()
+        if self.match and self.active:
+            self.board[move[0]][move[1]] = self.current_player
+            self.turn += 1
+            result = self.gameBoardStatus(move)
+            if result == self.EMPTY:
+                self.switchPlayer()
+            else:
+                self.match = False
