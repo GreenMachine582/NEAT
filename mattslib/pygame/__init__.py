815fcdb58b4c03c5df8bbff70fe4216499c03781
Matthew <greenchicken1902@gmail.com>
2022-04-21 18:39:27 +1000

    Changed colours from lists to tuples.

--- mattslib/pygame/__init__.py
+++ mattslib/pygame/__init__.py
@@ -9,29 +9,29 @@ __version__ = '1.2'
 
 
 # Colors
-BLACK = [0, 0, 0]
-DARK_GRAY = [40, 40, 40]
-GRAY = [100, 100, 100]
-LIGHT_GRAY = [200, 200, 200]
-WHITE = [255, 255, 255]
-DARK_RED = [200, 0, 0]
-RED = [255, 0, 0]
-DARK_GREEN = [0, 140, 0]
-GREEN = [0, 255, 0]
-LIGHT_GREEN = [100, 255, 100]
-DARK_BLUE = [0, 0, 140]
-BLUE = [0, 0, 255]
-DARK_YELLOW = [200, 200, 0]
-YELLOW = [255, 255, 0]
+BLACK = (0, 0, 0)
+DARK_GRAY = (40, 40, 40)
+GRAY = (100, 100, 100)
+LIGHT_GRAY = (200, 200, 200)
+WHITE = (255, 255, 255)
+DARK_RED = (200, 0, 0)
+RED = (255, 0, 0)
+DARK_GREEN = (0, 140, 0)
+GREEN = (0, 255, 0)
+LIGHT_GREEN = (100, 255, 100)
+DARK_BLUE = (0, 0, 140)
+BLUE = (0, 0, 255)
+DARK_YELLOW = (200, 200, 0)
+YELLOW = (255, 255, 0)
 
 
-def changeColour(colour: list, change_by: Any) -> list:
+def changeColour(colour: tuple, change_by: Any) -> tuple:
     """
     Changes the given colour with value, the clamps the colour ranges.
-    :param colour: list[int]
+    :param colour: tuple[int, int, int]
     :param change_by: Any
     :return:
-        - new_colour - list[int]
+        - new_colour - tuple[int, int, int]
     """
     if isinstance(change_by, int):
         new_colour = [colour[i] + change_by for i in range(len(colour))]
@@ -41,4 +41,4 @@ def changeColour(colour: list, change_by: Any) -> list:
         return colour
     for i in range(len(colour)):
         new_colour[i] = 0 if new_colour[i] < 0 else 255 if new_colour[i] > 255 else new_colour[i]
-    return new_colour
+    return tuple(new_colour)
